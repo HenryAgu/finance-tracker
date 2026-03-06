ac016ffb6868517075e9cb0a7da59f9e65b582e5
Henry Agu <aguhenrychuks@gmail.com>
2026-03-06 22:34:40 +0100

    refactor: made colors variables

--- components/footer.tsx
+++ components/footer.tsx
@@ -30,16 +30,16 @@ const Footer = () => {
             <div className="flex lg:flex-row flex-col gap-y-5 justify-between">
                 <Image src="/logo.svg" alt='footer-logo' width={145} height={24} className='aspect-145/24' />
                 <div className="flex flex-col gap-y-0.5 lg:items-end">
-                    <p className='text-sm font-normal text-[#64748B]'>© 2026 FinanceTracker App. All rights reserved.</p>
-                    <div className="flex items-center gap-x-1 text-[#94A3B8]">
+                    <p className='text-sm font-normal text-slate-500'>© 2026 FinanceTracker App. All rights reserved.</p>
+                    <div className="flex items-center gap-x-1 text-slate-400">
                         <LockKeyhole size={12} />
                         <p className='text-xs font-normal'>Secure local storage: All data is stored privately on your machine.</p>
                     </div>
                 </div>
             </div>
-            <div className="flex items-center pt-3 lg:pt-6 gap-x-3 lg:gap-x-6">
+            <div className="flex items-center lg:justify-start justify-center pt-3 lg:pt-6 gap-x-3 lg:gap-x-6">
                 {footerMenu.map((item) => (
-                    <Link href={item.path} key={item.title} className='text-sm font-normal text-[#94A3B8]'>{item.title}</Link>
+                    <Link href={item.path} key={item.title} className='text-sm font-normal text-slate-400'>{item.title}</Link>
                 ))}
             </div>
         </footer>
